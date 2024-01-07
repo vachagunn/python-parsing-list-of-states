be3dcb315ee0401b7ddc290ae09fd9b25f27cc99
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-07 13:25:29 +0500

    Обработка значений со словами 'тыс', 'млн', 'млрд'.

--- parse_wiki_load.py
+++ parse_wiki_load.py
@@ -5,11 +5,17 @@ import re
 
 def edit(value):
     string = "".join(str(value).split()).replace(',', '.')
-    pattern = r"\d+[\s,]?\d*[\s,]?\d*"
+    pattern = r"\d+[.]?\d*[.]?\d*"
     result = re.search(pattern, string)
 
     if result:
         result = float(result[0])
+        if 'тыс' in string:
+            result *= 10 ** 3
+        if 'млн' in string:
+            result *= 10 ** 6
+        if 'млрд' in string:
+            result *= 10 ** 9
 
     return result
 
@@ -18,6 +24,7 @@ with open('countries_params.db', 'rb') as file:
     data = pickle.load(file)
 
 pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
 data['T'] = data['Территория государства'].apply(edit)
 
