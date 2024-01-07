db55bb98d4e5f5bcef0c3e0c147ff764901e1298
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-07 13:01:33 +0500

    Проверка типов данных для значений в столбце T - "Территория государства".

--- parse_wiki_load.py
+++ parse_wiki_load.py
@@ -2,11 +2,18 @@ import pickle
 import pandas as pd
 import re
 
+
+def edit(x):
+    return type(x)  # не все значения string, есть none и float
+
+
 with open('countries_params.db', 'rb') as file:
     data = pickle.load(file)
 
 pd.set_option('display.max_columns', None)
 
+data['T'] = data['Территория государства'].apply(edit)
 
-print(data[['Территория государства', 'Население']])
+# print(data[['Территория государства', 'Население']])
 # print(data.info())
+print(data['T'])
